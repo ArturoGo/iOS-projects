664512f850a113f77237ec264ca2c33affcdb29d
Arturo Guzman <Aguzman@pclocal-2.local>
2018-08-04 20:40:08 -0500

    Initial Commit

---
+++ iamrich/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Arturo Guzman on 04/08/18.
+//  Copyright © 2018 Arturo Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
