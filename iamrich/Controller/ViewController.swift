b415167937b6d0fd95817c51cd8f849ae5eb77ed
Arturo Guzman <Aguzman@pclocal-2.local>
2018-08-06 17:18:18 -0500

    Cambios de ios project

--- iamrich/Controller/ViewController.swift
+++ iamrich/Controller/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Propertys
+    
+    @IBOutlet var labelTitle: UILabel!
+    
+    @IBOutlet var imageViewDiamont: UIImageView!
+    
+    @IBOutlet var buttonPush: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //Methods
+    @IBAction func buttonPressed(_ sender: UIButton) {
+       // self.labelTitle.text = "He pulsado el botón"
+        
+       // self.labelTitle.textColor = UIColor.green
+        // self.labelTitle.font = UIFont.systemFont(ofSize: 24.0)
+        
+        let controller = UIAlertController(title: "I am rich", message: """
+         I am Rich,
+         I deserve it.
+         I am good.
+         Healthy and succesfull
+         """, preferredStyle: .actionSheet)
+        
+        let action = UIAlertAction(title: "Aceptar", style: .default) { (action) in
+            print("He pulsado el boton aceptar")
+        }
+        controller.addAction(action)
+        
+        let action2 = UIAlertAction(title: "Borrar", style: .destructive, handler: {(action2) in
+            print("He pulsado el boton Borrar")
+        })
+        controller.addAction(action2)
+        
+        let action3 = UIAlertAction(title: "Cancelar", style: .cancel){_ in
+         print("He pulsado el botón cancelar")
+        }
+        controller.addAction(action3)
+        
+        self.show(controller, sender: nil)
+        
+    }
+    
 }
 
